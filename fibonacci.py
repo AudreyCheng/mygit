aa1b403a3981acac56af6deb524fab0da1754c61
cyy1204836529@163.com <AudreyC@synnex.com>
2018-12-28 16:37:30 +0800

    add a file

---
+++ fibonacci.py
@@ -0,0 +1,14 @@
+def fibonacci(n):
+    f_list = [1, 1]
+    if 2 >= n > 0:
+        return f_list[0:n]
+    for i in range(2, n):
+        f_list.append(f_list[-1] + f_list[-2])
+    return f_list
+
+f_list = fibonacci(8)
+print f_list
+
+
+
+
