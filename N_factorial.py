9351b3972e03e76fd8c64001868f5d705e244cd2
cyy1204836529@163.com <AudreyC@synnex.com>
2019-01-02 15:56:48 +0800

    modify a files

--- N_factorial.py
+++ N_factorial.py
@@ -10,28 +10,4 @@ def fac(n):
             break
         else:
             print fac(a)
-
-s = 0
-
-
-def fac_sum(n):
-    for i in range(1, n+1):
-        s = s + fac(i)
-        return s
-
 print fac(4)
-# print fac_sum(4)
-
-
-
-
-
-# def n_factorial(n):
-#     result = 1
-#     i = 1
-#     for i in range(n):
-#         i = i+1
-#         result = i*(i+1)
-#         return result
-#
-# print n_factorial(4)
