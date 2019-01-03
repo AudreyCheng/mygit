264f575ad461b7ab0b94ea86d7d3f2434d94af42
cyy1204836529@163.com <AudreyC@synnex.com>
2019-01-03 17:06:59 +0800

    delete sort.py and add bubble_sort.py

---
+++ bubble_sort.py
@@ -0,0 +1,14 @@
+array = [9,1,5,3,2,8,0,7]
+
+
+def bubble_sort(array):
+    n = len(array)
+    for i in range(n-1):
+        for j in range(n-1-i):
+            if array[j] > array[j+1]:
+                array[j], array[j+1] = array[j+1], array[j]
+
+bubble_sort(array)
+print array
+
+
