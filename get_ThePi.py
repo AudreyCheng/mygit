c19b3783e0304f3e163ed13ee3cd158778ef2e98
cyy1204836529@163.com <AudreyC@synnex.com>
2019-01-04 17:51:59 +0800

    odify the get_Thepi

--- get_ThePi.py
+++ get_ThePi.py
@@ -1,13 +1,14 @@
 import random
 
-def get_pi(n):
-    inner = 0.0
 
-    for i in range(n) :
+def get_pi(n):
+    inner = 0
+
+    for i in range(n):
         x = random.random()
         y = random.random()
 
-        if x ** 2 + y ** 2 <= 1 :
+        if x ** 2 + y ** 2 <= 1:
             inner += 1
 
     pi = 4 * inner / n
